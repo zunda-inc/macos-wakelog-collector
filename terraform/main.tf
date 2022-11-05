bd60cbf353ccda704982a55322065c45a40ec867
Sho Sawada <sawada.sho@zunda.co.jp>
2022-11-05 14:42:06 +0900

    lambda example

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,3 +6,9 @@ resource "aws_s3_bucket_acl" "log_upload_bucker_acl" {
   bucket = aws_s3_bucket.log_upload_bucket.id
   acl = "private"
 }
+
+module "upload_ticket_server" {
+  source = "./modules"
+  namespace = var.namespace
+  upload_s3_bucket_arn = aws_s3_bucket.log_upload_bucket.arn
+}
